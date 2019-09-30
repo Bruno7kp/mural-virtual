62cc640df85d868e85a11588c1d82c5de5bd9d86
Érico Detoffol <ericodetoffol@uniplaclages.edu.br>
2019-09-30 20:55:51 -0300

    Estrutura css close #11

--- mural/mod_home/home.py
+++ mural/mod_home/home.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 
 bp_home = Blueprint('home', __name__, url_prefix='/', template_folder='templates')
 
-@bp_home.route("/home")
+@bp_home.route("/")
 def home():
     return render_template("home.html")
