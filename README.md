6b5d650befefda86aceedb67a88b4dc96088ed34
Bruno <bruno7kp@outlook.com>
2019-09-30 21:26:40 -0300

    Corrige o ambiente virtual

--- README.md
+++ README.md
@@ -12,10 +12,9 @@ Trabalho integrador da 4ª fase do curso de Sistemas de Informação da UNIPLAC 
 ### Instalação
 ```
 git clone https://github.com/bruno7kp/mural-virtual.git
-cd mural
 python -m venv venv
 venv\Scripts\activate
-pip install -r requirements.txt
+pip install -r mural\requirements.txt
 set FLASK_ENV='development'
 set FLASK_DEBUG=1
 python -m flask run
--- mural/app.py
+++ mural/app.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 
 from flask import Flask, redirect, url_for, render_template
-from .mod_home.home import bp_home
-from .mod_noticias.noticias import bp_noticias
-from .mod_avisos.avisos import bp_avisos
-from .mod_anuncios.anuncios import bp_anuncios
-from .mod_dashboard.dashboard import bp_dashboard
+from mural.mod_home.home import bp_home
+from mural.mod_noticias.noticias import bp_noticias
+from mural.mod_avisos.avisos import bp_avisos
+from mural.mod_anuncios.anuncios import bp_anuncios
+from mural.mod_dashboard.dashboard import bp_dashboard
 
 app = Flask(__name__)
 
